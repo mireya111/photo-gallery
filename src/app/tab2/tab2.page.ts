d52dee4e17ae4c1a1764e5eb2d88776e7786a9f4
mireya111 <mireya.garcia@epn.edu.ec>
2025-04-22 16:50:20 -0500

    Proyecto_camara

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PhotoService} from '../services/photo.service'
 
 @Component({
   selector: 'app-tab2',
@@ -7,7 +8,8 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  constructor(public photoService: PhotoService) {}
+  addPhotoGallery(){
+    this.photoService.addNewToGallery();
+  }
 }
